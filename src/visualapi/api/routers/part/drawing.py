21e9a6f3ca882a5bad4483fd6d34ccf67ca5db21
Jakub Nedoma <nedoma.jakub@gmail.com>
2025-08-19 13:09:27 +0200

    modified drawing endpoint for mapping

--- src/visualapi/api/routers/part/drawing.py
+++ src/visualapi/api/routers/part/drawing.py
@@ -4,6 +4,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import FileResponse
 from fastapi_cache.decorator import cache
 from sqlmodel import Session, select
+import util
+from loguru import logger
 
 from db import get_db
 from models.part import Part
@@ -25,14 +27,18 @@ def get_drawing(part_id: str, db_session: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail=f"Part {part_id} not found")
 
     if result.drawing_file:
-        drawing_path = Path(result.drawing_file)
-        if not drawing_path.exists():
+        original_drawing_path = Path(result.drawing_file)
+        mapped_drawing_path = util.map_network_to_local(original_drawing_path)
+
+        logger.debug(f"Mapped drawing path for part {part_id}: {mapped_drawing_path}")
+
+        if not mapped_drawing_path.exists():
             raise HTTPException(
                 status_code=404, detail=f"Drawing file for part {part_id} not found"
             )
 
         return FileResponse(
-            drawing_path, media_type="application/pdf", filename=drawing_path.name
+            mapped_drawing_path, media_type="application/pdf", filename=mapped_drawing_path.name
         )
 
     else:
