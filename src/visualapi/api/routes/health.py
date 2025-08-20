26466fe8346e37c8eded6acc51abe7fcf44ea5d9
Jakub Nedoma <nedoma.jakub@gmail.com>
2025-08-20 21:28:47 +0200

    heath check endpoint

---
+++ src/visualapi/api/routes/health.py
@@ -0,0 +1,10 @@
+"""Health check route for the API."""
+
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/utils", tags=["Utils"])
+
+
+@router.get("/health-check/")
+async def health_check() -> bool:
+    return True
