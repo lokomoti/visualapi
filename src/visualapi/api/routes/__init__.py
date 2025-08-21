ab2cec8c4276c56c39bcbd5d2dbbca9880f558ff
Jakub Nedoma <nedoma.jakub@gmail.com>
2025-08-21 15:36:03 +0200

    added healthcheck router

--- src/visualapi/api/routes/__init__.py
+++ src/visualapi/api/routes/__init__.py
@@ -8,6 +8,7 @@ from api.routes.customerorder import router as customerorder_router
 from api.routes.customerorderline import router as customerorderline_router
 from api.routes.part import router as part_router
 from api.routes.workorder import router as workorder_router
+from api.routes.health import router as health_router
 
 router = APIRouter(prefix="/api")
 
@@ -16,3 +17,4 @@ router.include_router(part_router)  # Exluded auth for allowing signed url endpo
 router.include_router(customerorder_router, dependencies=[Depends(authorize)])
 router.include_router(customerorderline_router, dependencies=[Depends(authorize)])
 router.include_router(customer_router, dependencies=[Depends(authorize)])
+router.include_router(health_router)
